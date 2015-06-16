a8639289279bb0d5ca14169ddf3ec8ae4091d200
laboratorio <laboratorio@atl-labobd-314>
2015-06-15 21:32:38 -0500

    clase numero dos

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+
+
+public class JavaApplication1 {
+
+    private int contador;
+    public static void main(String[] args) {
+        JavaApplication1 obj = new JavaApplication1();
+        for (int contador = 0; contador < 10; contador++) {
+            System.out.println("Imprimiendo el contador "+ contador);
+        }
+    }
+    
+}
